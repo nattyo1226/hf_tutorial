97a41d0644e3d3e8fae261f6d8f360dd87c1dcd8
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2025-11-10 23:36:02 +0900

    ♻️ Update cell structure

--- marimo/autoclass.py
+++ marimo/autoclass.py
@@ -1,28 +1,25 @@
 import marimo
 
-__generated_with = "0.17.2"
+__generated_with = "0.17.6"
 app = marimo.App(width="medium")
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     # Load pretrained instances with an AutoClass
 
     本チュートリアルでは、`AutoClass` を用いて、モデルから目的のアーキテクチャをロードする手法を学びます。
     `pipeline()` が事前処理・推論・事後処理を一挙に行うモデル全体を提供していたのとは対照的に、`AutoClass` を用いることで、モデルを構成する各アーキテクチャを選択的にロードし、利用することができます。
 
     本チュートリアルは、[Hugging Face Tranformers チュートリアル](https://huggingface.co/docs/transformers/v4.57.0/ja/autoclass_tutorial) を元に、一部加筆・修正して作成しています。
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Dependencies
 
     このチュートリアルコードをすべて実行するためには、明示的に `import` するライブラリの他に、以下のソフトウェアが必要です。
@@ -30,8 +27,7 @@ def _(mo):
     - [`tesseract`](https://github.com/tesseract-ocr/tesseract) (および、その Python ラッパー: `pytesseract`): 画像処理 (ocr)
     - `torch` ライブラリ or `tensorflow` ライブラリ: バックエンド
         - 本チュートリアルでは `torch` を用いるコードしか紹介しません
-    """
-    )
+    """)
     return
 
 
@@ -77,8 +73,7 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## `🤗 Tranformers` architecture
 
     一般的に、`🤗 Transformers` が提供するモデルは、次のような部品から構成されています。
@@ -88,22 +83,19 @@ def _(mo):
     - head: model の出力をタスク固有の出力に変換するアーキテクチャ
 
     `pipeline()` ではこれらをまとめてロードしていましたが、`AutoClass` はこれらをより細かい単位でロードする仕組みが用意されています。
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoConfig
 
     model に関する情報は、`config.json` に保存されています。
     これらの情報を引き出すために、`AutoConfig` クラスを用います。
     `AutoConfig.from_pretrained("model")` とすることで、上述の `config.json` がダウンロードされ、その内容が格納された `xxxConfig` クラスのインスタンスが作成されます。
-    """
-    )
+    """)
     return
 
 
@@ -119,8 +111,7 @@ def _(AutoConfig):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoTokenizer
 
     **トーカナイザ**は、入力テキストに対してトークン化・テンソル化などの変換を行うアーキテクチャです。
@@ -129,8 +120,7 @@ def _(mo):
     トーカナイザのロードには、`AutoTokenizer` クラスを用います。
     `AutoTokenizer` をはじめとする processor アーキテクチャ用のクラスには、`from_pretrained` メソッドが用意されています。
     `AutoTokenizer.from_pretrained("model name")` により、上述の config ファイルが読み込まれ、`xxxTokenizer` インスタンスが作成されます。
-    """
-    )
+    """)
     return
 
 
@@ -141,7 +131,11 @@ def _(AutoTokenizer):
 
     tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
     print(tokenizer)
+    return (tokenizer,)
 
+
+@app.cell
+def _(tokenizer):
     sequence = "In a hole in the ground there lived a hobbit."
     print(tokenizer(sequence))
     return
@@ -149,25 +143,27 @@ def _(AutoTokenizer):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoImageProcessor
 
     画像プロセッサは、入力画像にサイズ変更・正規化・テンソル化などの変換を行うアーキテクチャです。
     ここでは `AutoImageProcessor` クラスを用います。
-    """
-    )
+    """)
     return
 
 
 @app.cell
-def _(AutoImageProcessor, Image, io, requests):
+def _(AutoImageProcessor):
     # model: "google/vit-base-patch16-224" (86.6M params)
     # ref: https://huggingface.co/google/vit-base-patch16-224
 
     image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
     print(image_processor)
+    return (image_processor,)
 
+
+@app.cell
+def _(Image, image_processor, io, requests):
     image1_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg"
     image1 = Image.open(io.BytesIO(requests.get(image1_url).content))
     print(image_processor(image1))
@@ -176,21 +172,19 @@ def _(AutoImageProcessor, Image, io, requests):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoFeatureExtractor
 
     特徴抽出器は、入力の画像や動画に対して、一定の方法で特徴を抽出し、正規化・テンソル化などの変換を行うアーキテクチャです。
     ここでは、`AutoFeatureExtractor` クラスを用います。
 
     <audio src="https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac" controls></audio>
-    """
-    )
+    """)
     return
 
 
 @app.cell
-def _(AutoFeatureExtractor, io, librosa, requests):
+def _(AutoFeatureExtractor):
     # model: "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition" (316M params)
     # ref: https://huggingface.co/ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition
 
@@ -198,7 +192,11 @@ def _(AutoFeatureExtractor, io, librosa, requests):
         "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition",
     )
     print(feature_extractor)
+    return (feature_extractor,)
 
+
+@app.cell
+def _(feature_extractor, io, librosa, requests):
     target_sr = feature_extractor.sampling_rate
     speech_url = "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac"
     speech_bytes = io.BytesIO(requests.get(speech_url).content)
@@ -213,27 +211,29 @@ def _(AutoFeatureExtractor, io, librosa, requests):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoProcessor
 
     `Transformers` において、プロセッサとはマルチモーダルモデルの入力に対して前処理を行うアーキテクチャを指します。
     ここでは、`AutoProcessor` クラスを用います。
 
     <img src="https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png" width="30%">
-    """
-    )
+    """)
     return
 
 
 @app.cell
-def _(AutoProcessor, Image, io, requests):
+def _(AutoProcessor):
     # model: "microsoft/layoutlmv2-base-uncased" (200M params)
     # ref: https://huggingface.co/microsoft/layoutlmv2-base-uncased
 
     processor = AutoProcessor.from_pretrained("microsoft/layoutlmv2-base-uncased")
     print(processor)
+    return (processor,)
 
+
+@app.cell
+def _(Image, io, processor, requests):
     image2_url = "https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png"
     image2 = Image.open(io.BytesIO(requests.get(image2_url).content)).convert("RGB")
     text = ["invoice", "number"]
@@ -243,8 +243,7 @@ def _(AutoProcessor, Image, io, requests):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoModel
 
     model をロードするためには、`AutoModel` クラスを用います。
@@ -255,8 +254,7 @@ def _(mo):
 
     `from_pretrained` メソッドは、モデル名を受け取り、事前学習済みのパラメータをロードして、`xxxModel` を組み立てます。
     事前学習済み model を利用して、推論やファインチューニングを行うには、こちらを利用することになります。
-    """
-    )
+    """)
     return
 
 
@@ -276,14 +274,12 @@ def _(AutoModel):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## AutoModelFor
 
     model と同時に head もロードするためには、`AutoModelFor` クラス (`PyTorch` バックエンド) または `TFAutoModelFor` クラス (`tensorflow` バックエンド) を用います。
     ここでも、`AutoModel` と同様に、`from_config` メソッドと `from_pretrained` メソッドが用意されています。
-    """
-    )
+    """)
     return
 
 
