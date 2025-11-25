7038818581aa50bdad11312bb4b93532b4408642
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2025-11-25 15:53:16 +0900

    🐛 Fix

--- script/job.sh
+++ script/job.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 #------ qsub option --------#
-#PBS -q small-g
+#PBS -q regular-g
 #PBS -l select=1
 #PBS -l walltime=1:00:00
 #PBS -W group_list=gq42
 #PBS -j oe
 
 #------- Program execution -------#
-uv run src/finetune_trainer.py
+cd ${PBS_O_WORKDIR}
+uv run ./src/finetune_trainer.py
